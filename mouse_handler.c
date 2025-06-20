c6398cdacce8a2f75ba1e89431c67b3b7a7dc185
Kaisei Suzuki <kaisuzuk@c2r2s7.42tokyo.jp>
2025-06-20 11:43:41 +0900

    とりあえずズームすることに成功

--- mouse_handler.c
+++ mouse_handler.c
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 21:34:14 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/06/20 00:33:30 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/06/20 11:43:02 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,6 @@ void update_viewport(t_window *win)
 	win->y_max = 1.5 * scale + win->offset_y;
 }
 
-// void zoom(t_data *data, double scale_per, int x, int y)
-// {
-// 	t_window *win;
-
-// 	win = data->win;
-	
-// 	t_complex mouse_abi = pixel_to_complex(win, x, y);
-	
-// 	win->zoom *= scale_per;
-
-// 	win->offset_x = mouse_abi.a - ((win->x_max - win->x_min) / win->width) * x;
-// 	win->offset_y = mouse_abi.bi - ((win->y_max - win->y_min) / win->height) * y;
-	
-// 	update_viewport(win);
-// 	render_mandelbrot(data->win, data->img);
-// }
-
 void zoom(t_data *data, double scale_per, int x, int y)
 {
 	t_window *win;
@@ -51,7 +34,8 @@ void zoom(t_data *data, double scale_per, int x, int y)
 	double old_height;
 	double new_width;
 	double new_height;
-
+	double raito_x;
+	double raito_y;
 	win = data->win;
 	mouse = pixel_to_complex(win, x, y);
 
@@ -62,11 +46,17 @@ void zoom(t_data *data, double scale_per, int x, int y)
 	
 	new_width = old_width / scale_per;
 	new_height = old_height / scale_per;
+
+	raito_x = x / (double)win->width;
+	raito_y = y / (double)win->height;
+
+	win->x_min = mouse.a - raito_x * new_width;
+	win->x_max = win->x_min + new_width;
+
+	win->y_max = new_height * raito_y + mouse.bi;
+	win->y_min = win->y_max - new_height;
 	
-	win->x_min = mouse.a - (new_width / 2);
-	win->x_max = mouse.a + (new_width / 2);
-	win->y_min = mouse.bi - (new_height / 2);
-	win->y_max = mouse.bi + (new_height / 2);
+	
 	render_mandelbrot(win, data->img);
 }
 
